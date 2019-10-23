9f1bde443cfe31a8b4175688eb860594c0b19938
Kwabena Asante-Poku <kwabena.a92@gmail.com>
2019-10-23 21:38:06 +0200

    Refactor Main java file into more functions

--- Solution/src/main/java/se/kth/sda/Main.java
+++ Solution/src/main/java/se/kth/sda/Main.java
@@ -5,19 +5,46 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class Main {
     private static StringBuilder no_damage_from, no_damage_to, double_damage_from, double_damage_to, half_damage_from, half_damage_to;
 
     private static final String POKEMON_API_BASE_URL = "https://pokeapi.co/api/v2/";
     private static final String POKEMON_API_POKEMON_URL = POKEMON_API_BASE_URL + "pokemon/";
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        run();
+        boolean exit = false;
+
+
+        while (!exit) {
+            System.out.println("1- Information about a Pokemon");
+            System.out.println("2- Information about an Area");
+            System.out.println("3- Exit");
+
+            System.out.println("Enter a number: ");
+            String input = scanner.nextLine().strip();
+
+            switch (input) {
+                case "1":
+                    runOptionPokemonInformation();
+                    break;
+                case "2":
+                    runOptionLocationInformation();
+                    break;
+                case "3":
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Not Avaliable\n");
+                    break;
+            }
+        }
     }
 
     /**
@@ -29,14 +56,17 @@ public class Main {
         boolean inputValid = false;
 
         while (!inputValid) {
-            System.out.println("Enter the name of the Pokemon: ");
-            Scanner scanner = new Scanner(System.in);
-            String inputName = (scanner.nextLine()).toLowerCase().strip();
-            response = getInfo(POKEMON_API_POKEMON_URL + inputName);
+            System.out.println("Enter the name of the Pokemon or write '#' to go back: ");
+            String inputPokemonName = (scanner.nextLine()).toLowerCase().strip();
+            if (inputPokemonName.equals("#")) {
+                return;
+            }
+            response = getInfo(POKEMON_API_POKEMON_URL + inputPokemonName);
             if (response != null) {
                 inputValid = true;
+            } else {
+                System.out.println("Sorry, could not find the pokémon you were looking for :(");
             }
-
         }
 
         JSONObject pokemonInfo = new JSONObject(response);
@@ -45,173 +75,43 @@ public class Main {
         int weight = pokemonInfo.getInt("weight");
         int id = pokemonInfo.getInt("id");
 
-
-
-
-//        JSONObject obj2 = new JSONObject(getInfo("https://pokeapi.co/api/v2/type/"));
-//        JSONArray results = obj2.getJSONArray("results");
-//
-//
-//        for (int i = 0; i < results.length(); i++) {
-//            if ((results.getJSONObject(i).getString("name")).equals(type)) {
-//                JSONObject damaginObj = new JSONObject(getinfo(results.getJSONObject(i).getString("url"))).getJSONObject("damage_relations");
-//
-//
-//                no_damage_from = new StringBuilder();
-//                JSONArray no_damage_from_arr = damaginObj.getJSONArray("no_damage_from");
-//                if (no_damage_from_arr.length() != 0) {
-//                    for (int x = 0; x < no_damage_from_arr.length(); x++) {
-//                        no_damage_from.append(no_damage_from_arr.getJSONObject(x).getString("name"));
-//                        if (x != no_damage_from_arr.length() - 1) {
-//                            no_damage_from.append(",");
-//                        }
-//                    }
-//                } else {
-//                    no_damage_to.append("No One");
-//                }
-//
-//                no_damage_to = new StringBuilder();
-//                JSONArray no_damage_to_arr = damaginObj.getJSONArray("no_damage_to");
-//                if (no_damage_to_arr.length() != 0) {
-//                    for (int x = 0; x < no_damage_to_arr.length(); x++) {
-//                        no_damage_to.append(no_damage_to_arr.getJSONObject(x).getString("name"));
-//                        if (x != no_damage_to_arr.length() - 1) {
-//                            no_damage_to.append(",");
-//                        }
-//                    }
-//                } else {
-//                    no_damage_to.append("No One");
-//                }
-//
-//
-//                double_damage_from = new StringBuilder();
-//                JSONArray double_damage_from_arr = damaginObj.getJSONArray("double_damage_from");
-//                if (double_damage_from_arr.length() != 0) {
-//                    for (int x = 0; x < double_damage_from_arr.length(); x++) {
-//                        double_damage_from.append(double_damage_from_arr.getJSONObject(x).getString("name"));
-//                        if (x != double_damage_from_arr.length() - 1) {
-//                            double_damage_from.append(",");
-//                        }
-//                    }
-//                } else {
-//                    half_damage_from.append("No One");
-//                }
-//
-//
-//                double_damage_to = new StringBuilder();
-//                JSONArray double_damage_to_arr = damaginObj.getJSONArray("double_damage_to");
-//                if (double_damage_to_arr.length() != 0) {
-//                    for (int x = 0; x < double_damage_to_arr.length(); x++) {
-//                        double_damage_to.append(double_damage_to_arr.getJSONObject(x).getString("name"));
-//                        if (x != double_damage_to_arr.length() - 1) {
-//                            double_damage_to.append(",");
-//                        }
-//                    }
-//                } else {
-//                    double_damage_to.append("No One");
-//                }
-//
-//
-//                half_damage_from = new StringBuilder();
-//                JSONArray half_damage_from_arr = damaginObj.getJSONArray("half_damage_from");
-//                if (half_damage_from_arr.length() != 0) {
-//                    for (int x = 0; x < half_damage_from_arr.length(); x++) {
-//                        half_damage_from.append(half_damage_from_arr.getJSONObject(x).getString("name"));
-//                        if (x != half_damage_from_arr.length() - 1) {
-//                            half_damage_from.append(",");
-//                        }
-//                    }
-//
-//                } else {
-//                    half_damage_from.append("No One");
-//                }
-//
-//
-//                half_damage_to = new StringBuilder();
-//                JSONArray half_damage_to_arr = damaginObj.getJSONArray("half_damage_to");
-//                if (half_damage_to_arr.length() != 0) {
-//                    for (int x = 0; x < half_damage_to_arr.length(); x++) {
-//                        half_damage_to.append(half_damage_to_arr.getJSONObject(x).getString("name"));
-//                        if (x != half_damage_to_arr.length() - 1) {
-//                            half_damage_to.append(",");
-//                        }
-//                    }
-//                } else {
-//                    half_damage_to.append("No One");
-//                }
-//
-//            }
-//        }
-//
-//
         System.out.println("Name: " + name);
         System.out.println("Id: " + id);
         System.out.println("Height: " + height);
         System.out.println("Weight: " + weight);
-//
-//
-//        System.out.println("\n\n");
-//
-//        //Option 2
-
-
+        System.out.println("\n");
     }
 
-    static void run() {
+    /**
+     * This method asks the user for a location name and presents (prints) information about it (the area, the region it
+     * is in and the name.
+     */
+    static void runOptionLocationInformation() {
+        String response = null;
+        boolean inputValid = false;
 
-        while (true) {
-
-            System.out.println("1- Information about a Pokemon");
-            System.out.println("2- Information about an Area");
-            System.out.println("3- Exit");
-
-            Scanner input = new Scanner(System.in);
-            System.out.print("Enter a number: ");
-            int number = input.nextInt();
-
-            //Option 1
-            if (number == 1) {
-                runOptionPokemonInformation();
-            } else if (number == 2) {
-                System.out.print("Enter the name of Location: ");
-                Scanner s = new Scanner(System.in);
-                String location = (s.nextLine()).toLowerCase();
-                String response = getInfo("https://pokeapi.co/api/v2/location/" + location);
-
-                if (response == null) {
-                    continue;
-                }
-                JSONObject obj = new JSONObject(response);
-                String region = obj.getJSONObject("region").getString("name");
-
-                JSONArray arr = obj.getJSONArray("areas");
-                StringBuilder area = new StringBuilder();
-
-                for (int i = 0; i < arr.length(); i++) {
-                    area.append(arr.getJSONObject(i).getString("name"));
-                    if (i != arr.length() - 1) {
-                        area.append(",");
-                    }
-                }
-
-
-                System.out.println("Name: " + location);
-                System.out.println("Area: " + area);
-                System.out.println("Region: " + region);
-                System.out.println("\n\n");
-
-                //Optoin 3
-            } else if (number == 3) {
-                break;
-            } else {
-                System.out.println("Not Avaliable");
-                System.out.println("\n\n");
-
+        while (!inputValid) {
+            System.out.println("Enter the name of Location: ");
+            String inputLocation = (scanner.nextLine()).toLowerCase();
+            response = getInfo("https://pokeapi.co/api/v2/location/" + inputLocation);
+            if (response != null) {
+                inputValid = true;
             }
-
-
         }
 
+        JSONObject locationInfo = new JSONObject(response);
+        JSONArray nameInfos = locationInfo.getJSONArray("names");
+        Stream<JSONObject> namesStream = StreamSupport.stream(nameInfos.spliterator(), false).map(o -> (JSONObject) o);
+        String names = namesStream.map(
+                j -> "(" + j.getJSONObject("language").getString("name") + ") "
+                        + j.getString("name"))
+                .collect(Collectors.joining(" / "));
+        String region = locationInfo.getJSONObject("region").getString("name");
+
+
+        System.out.println("Name(s): " + names);
+        System.out.println("Region: " + region);
+        System.out.println("\n");
     }
 
     //returned value from API as String
@@ -220,10 +120,10 @@ public class Main {
         try {
 
             URL url = new URL(urli);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-            int responseCode = conn.getResponseCode();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int responseCode = connection.getResponseCode();
 
             // Code 200 is the standard response for get requests in the HTTP protocol.
             if (responseCode != 200) {
@@ -231,16 +131,11 @@ public class Main {
                         "Code: " + responseCode);
                 return null;
             } else {
-                Scanner sc = new Scanner(conn.getInputStream());
-                while (sc.hasNext()) {
-                    response = sc.nextLine();
+                Scanner responseScanner = new Scanner(connection.getInputStream());
+                while (responseScanner.hasNext()) {
+                    response = responseScanner.nextLine();
                 }
-                sc.close();
             }
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
