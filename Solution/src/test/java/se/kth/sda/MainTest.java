b728fd8cce6fab035c6e76ef5f6cea0a0671445c
Kwabena Asante-Poku <kwabena.a92@gmail.com>
2019-10-23 21:38:48 +0200

    Add tests for new functions

--- Solution/src/test/java/se/kth/sda/MainTest.java
+++ Solution/src/test/java/se/kth/sda/MainTest.java
@@ -1,24 +1,24 @@
 package se.kth.sda;
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
 
 public class MainTest {
 
     /**
-     * This test tries out the method testRunOptionPokemonInfo. The method relies on user input so we have to simulate
-     * it.
+     * Helper method to exchange the scanner in Main so that it reads from the string input of this method rather than
+     * the console. This is so that we can simulate user input.
+     *
+     * @param input
      */
-    @Test
-    public void testRunOptionPokemonInfo() {
-
-        // Here we are simulating user input by making it so that java doesn't read from the console, but from our
-        // own input stream, which is fed the string "pikachu".
-        String data = "pikachu";
-        System.setIn(new ByteArrayInputStream(data.getBytes()));
-        Main.runOptionPokemonInformation();
+    private void setUpMainScanner(String input) {
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        Main.scanner = new Scanner(inputStream);
     }
 
     /**
@@ -26,13 +26,37 @@ public class MainTest {
      * it.
      */
     @Test
-    public void testRunOptionPokemonInfoBadInput() {
+    public void testRunOptionPokemonInfo() {
+        setUpMainScanner("pikachu");
 
-        // Here we are simulating user input by making it so that java doesn't read from the console, but from our
-        // own input stream, which is fed the string "pikachu".
-        String data = "awfeaf";
-        System.setIn(new ByteArrayInputStream(data.getBytes()));
         Main.runOptionPokemonInformation();
+    }
+
+    /**
+     * This test tries out the method testRunOptionPokemonInfo with a pokémon name that shouldn't exist. The method
+     * relies on user input so we have to simulate it.
+     */
+    @Test
+    public void testRunOptionPokemonInfoBadInput() {
+        String userInput = "wadwad" +
+                "\n#";
+        setUpMainScanner(userInput);
+
+        Main.runOptionPokemonInformation();
+    }
+
+    /**
+     * This test tries out the method testRunOptionLocationInfo with location name. Since we don't have any return
+     * value from the method and it also don't change the state of any input variables we can't really make use of
+     * an assertEquals statement or similar. But what we can test is that for the given input it should terminate
+     * without error.
+     */
+    @Test
+    public void testRunOptionLocationInfo() {
+        String userInput = "canalave-city";
+        setUpMainScanner(userInput);
+
+        Main.runOptionLocationInformation();
     }
 
 }
